d84febb428ec071e0b022d189f929de1e43931c3
myNBGcode <developer@nbg.gr>
2019-05-10 08:13:45 +0300

    change sandbox version to latest
    change sandbox version to latest

--- routes/myAccounts.js
+++ routes/myAccounts.js
@@ -8,7 +8,7 @@ var request = require("request");
 router.get('/myAccounts', function(req, res, next) {
  var str='';
 var options = { method: 'GET',
-  url: 'https://apis.nbg.gr/public/sandbox/obp.account.sandbox/v1.1/obp/my/accounts',
+  url: 'https://apis.nbg.gr/sandbox/obpaccount/headers/v1.3/obp/my/accounts',
   headers:
    {accept: 'text/json',
    request_id: 'REPLACE_THIS_VALUE',
@@ -17,7 +17,7 @@ var options = { method: 'GET',
    provider_username: 'REPLACE_THIS_VALUE',
    application_id: 'REPLACE_THIS_VALUE',
    sandbox_id: 'REPLACE_THIS_VALUE',
-   'x-ibm-client-id': 'REPLACE_THIS_VALUE' } };
+   'Client-Id': 'REPLACE_THIS_VALUE' } };
 
 request(options, function (error, response, body) {
   if (error) return console.error('Failed: %s', error.message);
