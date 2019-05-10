84030674baa15cfbcdc7c7fd28d53a2db7cacf03
myNBGcode <developer@nbg.gr>
2019-05-10 08:13:48 +0300

    change sandbox version to latest
    change sandbox version to latest

--- routes/deletesandbox.js
+++ routes/deletesandbox.js
@@ -8,7 +8,7 @@ var request = require("request");
 router.get('/deletesandbox', function(req, res, next) {
     var str='';
 var options = { method: 'DELETE',
-  url: 'https://apis.nbg.gr/public/sandbox/obp.account.sandbox/v1.1/sandbox/REPLACE_THIS_VALUE1',
+  url: 'https://apis.nbg.gr/sandbox/obpaccount/headers/v1.3/sandbox/REPLACE_THIS_VALUE',
   headers: { accept: 'text/json',
   'content-type': 'text/json',
   request_id: 'REPLACE_THIS_VALUE',
@@ -17,7 +17,7 @@ var options = { method: 'DELETE',
   provider_username:'REPLACE_THIS_VALUE',
   application_id:'REPLACE_THIS_VALUE',
   //sandbox_id:'REPLACE_THIS_VALUE',
-  'x-ibm-client-id': 'REPLACE_THIS_VALUE'}, };
+  'Client-Id': 'REPLACE_THIS_VALUE'}, };
 
 request(options, function (error, response, body) {
   if (error) return console.error('Failed: %s', error.message);
