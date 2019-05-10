ee8e4268f9ba6d2ff4b8b2356e0c0af1663c46f5
myNBGcode <developer@nbg.gr>
2019-05-10 08:46:01 +0300

    fix headers
    fix headers

--- routes/createsandbox.js
+++ routes/createsandbox.js
@@ -11,11 +11,9 @@ var options = { method: 'POST',
   url: 'https://apis.nbg.gr/sandbox/obpaccount/headers/v1.3/sandbox',
   headers: { accept: 'text/json',
    'content-type': 'text/json',
-   sandbox_id:'REPLACE_THIS_VALUE',
    'Client-Id': 'REPLACE_THIS_VALUE'},
   //Replace the line below to your own sandbox id and comment the line below it
-  //body: { sandbox_id: 'REPLACE_THIS_VALUE' },
-  body: { sandbox_id: Date.now() },
+  body: { sandbox_id: 'REPLACE_THIS_VALUE' },
   json: true };
 
 request(options, function (error, response, body) {
