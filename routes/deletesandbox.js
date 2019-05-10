ec956cfa52ca6a252c33babb86f2c33561f967bc
myNBGcode <developer@nbg.gr>
2019-05-10 08:42:16 +0300

    headers fix
    headers fix

--- routes/deletesandbox.js
+++ routes/deletesandbox.js
@@ -11,12 +11,6 @@ var options = { method: 'DELETE',
   url: 'https://apis.nbg.gr/sandbox/obpaccount/headers/v1.3/sandbox/REPLACE_THIS_VALUE',
   headers: { accept: 'text/json',
   'content-type': 'text/json',
-  request_id: 'REPLACE_THIS_VALUE',
-  provider:'NBG',
-  provider_id:'NBG.gr',
-  provider_username:'REPLACE_THIS_VALUE',
-  application_id:'REPLACE_THIS_VALUE',
-  //sandbox_id:'REPLACE_THIS_VALUE',
   'Client-Id': 'REPLACE_THIS_VALUE'}, };
 
 request(options, function (error, response, body) {
